b0ebf5acab584833e428bcf9164dbe31a5c5dca1
chqiuu <35096687+chqiuu@users.noreply.github.com>
2020-12-23 11:14:34 +0800

    1. 增加Controller中URL映射名称显示功能，且可以自定义设置； 2. 增加生成代码时候数据校验功能；

--- src/components/GlobalFooter/index.vue
+++ src/components/GlobalFooter/index.vue
@@ -1,12 +1,12 @@
 <template>
   <global-footer class="footer custom-render">
     <template v-slot:links>
-      <a href="https://www.github.com/vueComponent/pro-layout" target="_blank">Pro Layout</a>
-      <a href="https://www.github.com/vueComponent/ant-design-vue-pro" target="_blank">Github</a>
-      <a href="https://www.github.com/sendya/" target="_blank">@Sendya</a>
+      <a href="https://github.com/chqiuu/code-generator-plus-ant-vue" target="_blank">前端界面</a>
+      <a href="https://github.com/chqiuu/code-generator-plus" target="_blank">Github</a>
+      <a href="https://github.com/chqiuu" target="_blank">@chqiuu</a>
     </template>
     <template v-slot:copyright>
-      <a href="https://github.com/vueComponent" target="_blank">vueComponent</a>
+      <a href="https://github.com/chqiuu" target="_blank">CHQIUU</a>
     </template>
   </global-footer>
 </template>
